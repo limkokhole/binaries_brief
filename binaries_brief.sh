f18026c0f68347617411d7dbdf8296444ea0be37
Kok Hole <kokhole.lim@selectv.co>
2018-01-18 18:08:20 +0800

    [1] shorten the filetype to reduce noise [2] spaces for each '--- file' to better spot on

--- binaries_brief.sh
+++ binaries_brief.sh
@@ -3,8 +3,8 @@
 #1st part: For every package name related to the valid directory files, the next line will shows its man, if any.
 #2nd part: --- valid directory files and the next line will shows its man, if any
 #3rd part: The last part will be package's description, home page, and maintainer contact.
-d='/etc/';
-f=~/Downloads/mybin_etc1.c;
+d='/usr/bin/';
+f=~/Downloads/mybin_usrbin_jan_18_2018.c;
 d=${d%/}'/';
 dc=${#d};
 ((dc++));
@@ -25,13 +25,9 @@ find "$d" -type f -maxdepth 1 -exec dpkg -S {} + 2> /dev/null | sort |
 		pkgb="$(echo $pkgbp | cut -c$dc-)";
 		((gn++));
 		if [ "$pkgp" == "$pkgn" ]; then
-			echo -n "--- $pkgb" >> "$f";
-			ft="$(file -n -b -e elf $pkgbp)";
-			if [ "${ft#ELF}" != "${ft}" ]; then
-				echo -e "\t\t(ELF)" >> "$f";
-			else
-				echo -e "\t\t($ft)" >> "$f";
-			fi
+			echo -en "\n--- $pkgb" >> "$f";
+			ft="$(file -n -b -e elf $pkgbp | cut -d' ' -f1)";
+			echo -e "\t\t($ft)" >> "$f";
 			man -f "$pkgb" 2>/dev/null >> "$f";
 			if [ "$total" == "$gn" ]; then
 				echo -en '\n\n\t\t\t\t' >> "$f";
@@ -49,13 +45,9 @@ find "$d" -type f -maxdepth 1 -exec dpkg -S {} + 2> /dev/null | sort |
 		echo -n "[$n] " >> "$f";
 		echo "$pkgn" >> "$f";
 		man -f "$pkgn" 2>/dev/null >> "$f";
-		echo -n "--- $pkgb" >> "$f";
-		ft="$(file -n -b -e elf $pkgbp)";
-		if [ "${ft#ELF}" != "${ft}" ]; then
-			echo -e "\t\t(ELF)" >> "$f";
-		else
-			echo -e "\t\t($ft)" >> "$f";
-		fi
+		echo -en "\n--- $pkgb" >> "$f";
+		ft="$(file -n -b -e elf $pkgbp | cut -d' ' -f1)";
+		echo -e "\t\t($ft)" >> "$f";
 		man -f "$pkgb" 2>/dev/null >> "$f";
 		if [ "$total" == "$gn" ]; then
 			echo -en '\n\n\t\t\t\t' >> "$f";
